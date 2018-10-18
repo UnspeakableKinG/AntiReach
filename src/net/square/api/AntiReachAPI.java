941950e170bf69e7906380d8348666473a1a96ec
Felix M <admin@squarecode.de>
2018-10-18 14:56:05 +0200

    new update

--- src/net/square/api/AntiReachAPI.java
+++ src/net/square/api/AntiReachAPI.java
@@ -10,21 +10,32 @@ import org.bukkit.entity.Player;
  */
 public class AntiReachAPI {
 
+    public static AntiReachAPI instance;
+
+    public void setInstance() {
+        instance = this;
+    }
+
     public static Integer getVL(final Player player) {
         return API.instance.VL.get(player.getUniqueId());
     }
+
     public static void createPlayer(final Player player) {
         API.instance.VL.put(player.getUniqueId(), 0);
     }
+
     public static Integer addVL(final Player player) {
         return API.instance.VL.put(player.getUniqueId(), API.instance.VL.get(player.getUniqueId()) +1);
     }
+
     public static void clearMap() {
         API.instance.VL.clear();
     }
+
     public static void removePlayer(final Player player) {
         API.instance.VL.remove(player.getUniqueId());
     }
+
     public static void setVL(final Player player, int level) {
         API.instance.VL.replace(player.getUniqueId(), API.instance.VL.get(player.getUniqueId()), level);
     }
