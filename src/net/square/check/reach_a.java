18a0d2b21c6e9c4a9b34ae2f462d418ec9119e8f
Felix M <admin@squarecode.de>
2018-10-23 23:13:54 +0200

    added Check and ReachType for more reach checks in the future

--- src/net/square/check/reach_a.java
+++ src/net/square/check/reach_a.java
@@ -12,7 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 public class reach_a extends Check {
 
